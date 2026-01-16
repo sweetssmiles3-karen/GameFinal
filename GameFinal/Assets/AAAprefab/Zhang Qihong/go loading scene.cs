b05d5d35094c4a7305e66eac91697a9e22523243
YUANBOYA251 <1539338252@qq.com>
2026-01-16 14:04:47 +0800

    26.1.16.01
    add the function to the start menu to go levels

--- GameFinal/Assets/AAAprefab/Zhang Qihong/go loading scene.cs
+++ GameFinal/Assets/AAAprefab/Zhang Qihong/go loading scene.cs
@@ -4,8 +4,8 @@ using UnityEngine.SceneManagement;
 public class CurrentSceneLoader : MonoBehaviour
 {
     public static CurrentSceneLoader Instance { get; private set; }
-    private const string NextSceneName = "NextLevel";
-    void Awake() => Instance = this;
+    public string NextSceneName;
+   // void Awake() => Instance = this;
 
     // 由过关检测函数调用
     public void TriggerLevelTransition()
